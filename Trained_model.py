e56844f199877676a50337bf6d686eb8160f5a5b
Ryan Tuscano <104095471+RyanTuscano@users.noreply.github.com>
2024-07-28 02:07:41 +0530

    Create Trained_model.py

---
+++ Trained_model.py
@@ -0,0 +1,4 @@
+The trained model with parameters was more than 25 MB, so wasn't directly able to post the file;
+Below is the drive link from where you can download the trained model :)
+
+"https://drive.google.com/file/d/1C1n5dbrjuui48MzzT9CP3b_7KFIbOQ5L/view?usp=sharing"
